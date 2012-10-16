20770041cc58d2ba327185e12304dff8456eb942
John Robeson <johnny@localmomentum.net>
2012-10-16 06:16:46 -0700

    add files from zen-cart-v1.5.1-full-fileset-09182012.zip

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-ZenCart
+A patched version of ZenCart for easier integration with external projects.
 =======
+
+The master version of this repository will be continually rebased on top of 
+the zip distribution files provided by ZenCart.
+
+It is currently based on ZenCart 1.5.1
+
+
+Modifications
+=======
+
---
+++ includes/init_includes/init_general_funcs.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * load the system wide functions
+ * see {@link  http://www.zen-cart.com/wiki/index.php/Developers_API_Tutorials#InitSystem wikitutorials} for more details.
+ *
+ * @package initSystem
+ * @copyright Copyright 2003-2012 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version GIT: $Id: Author: Ian Wilson  Tue Jul 24 11:36:47 2012 +0100 Modified in v1.5.1 $
+ */
+if (!defined('IS_ADMIN_FLAG')) {
+  die('Illegal Access');
+}
+/**
+ * General Functions
+ */
+require(DIR_WS_FUNCTIONS . 'functions_general.php');
+/**
+ * html_output functions (href_links, input types etc)
+ */
+require(DIR_WS_FUNCTIONS . 'html_output.php');
+/**
+ * basic email functions
+ */
+require(DIR_WS_FUNCTIONS . 'functions_email.php');
+/**
+ * EZ-Pages functions
+ */
+require(DIR_WS_FUNCTIONS . 'functions_ezpages.php');
+/**
+ * require the password crypto functions
+ */
+require(DIR_WS_FUNCTIONS . 'password_funcs.php');
+/**
+ * User Defined Functions
+ */
+include(DIR_WS_MODULES . 'extra_functions.php');
+?>
